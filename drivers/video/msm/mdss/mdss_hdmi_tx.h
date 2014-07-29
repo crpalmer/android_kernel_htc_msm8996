4256d80bb0569d14f6b78eb8690f54de633dd5c5
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2014-07-29 16:41:57 -0400

    msm: mdss: hdmi: add support for CEC suspend and resume events
    Add support for CEC commands to suspend and resume the device.
    The HDMI core must be kept on when the CEC wakeup feature is
    enabled and the device is going into suspend state. Furthermore,
    interrupts must be enabled in this state to capture CEC commands.
    This allows the device to be resumed later on via CEC wakeup commands.
    
    Change-Id: Ie6fcbc666e4f40335ab8faaa969d4b03aa83e17c
    Signed-off-by: Tatenda Chipeperekwa <tatendac@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.h
+++ drivers/video/msm/mdss/mdss_hdmi_tx.h
@@ -197,6 +197,7 @@ struct hdmi_tx_ctrl {
 	struct cec_cbs hdmi_cec_cbs;
 
 	char disp_switch_name[MAX_SWITCH_NAME_SIZE];
+	bool power_data_enable[HDMI_TX_MAX_PM];
 
 	u8 ds_hdcp;
 	struct mutex hpd_mutex;

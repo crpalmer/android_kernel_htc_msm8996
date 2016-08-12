21af838fb9f92eec75bdf755f88fe1bd4e8695bd
Arve Hjønnevåg <arve@android.com>
2016-08-12 16:04:28 -0700

    ANDROID: binder: Clear binder and cookie when setting handle in flat binder struct
    Prevents leaking pointers between processes
    
    BUG: 30768347
    Change-Id: Id898076926f658a1b8b27a3ccb848756b36de4ca
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Git-repo: https://android.googlesource.com/kernel/msm.git
    Git-commit: 11032d745836280574827bb1db5e64a94945180e
    Signed-off-by: Dennis Cagle <d-cagle@codeaurora.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1636,7 +1636,9 @@ static void binder_transaction(struct binder_proc *proc,
 				fp->type = BINDER_TYPE_HANDLE;
 			else
 				fp->type = BINDER_TYPE_WEAK_HANDLE;
+			fp->binder = 0;
 			fp->handle = ref->desc;
+			fp->cookie = 0;
 			binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE,
 				       &thread->todo);
 
@@ -1682,7 +1684,9 @@ static void binder_transaction(struct binder_proc *proc,
 					return_error = BR_FAILED_REPLY;
 					goto err_binder_get_ref_for_node_failed;
 				}
+				fp->binder = 0;
 				fp->handle = new_ref->desc;
+				fp->cookie = 0;
 				binder_inc_ref(new_ref, fp->type == BINDER_TYPE_HANDLE, NULL);
 				trace_binder_transaction_ref_to_ref(t, ref,
 								    new_ref);
@@ -1733,7 +1737,8 @@ static void binder_transaction(struct binder_proc *proc,
 			trace_binder_transaction_fd(t, fp->handle, target_fd);
 			binder_debug(BINDER_DEBUG_TRANSACTION,
 				     "        fd %d -> %d\n", fp->handle, target_fd);
-			
+			/* TODO: fput? */
+			fp->binder = 0;
 			fp->handle = target_fd;
 		} break;
 

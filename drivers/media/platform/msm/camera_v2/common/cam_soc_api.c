2ad6f22d22f5c0159b4c3c4a24305a26c81d0ae1
Christopher R. Palmer <crpalmer@gmail.com>
2016-11-30 14:21:58 -0500

    Revert "msm: camera: Change API to populate regulator name"
    This reverts commit 3c1161b6a4db1700a1b61e862a2de1094a0e92e6.

--- drivers/media/platform/msm/camera_v2/common/cam_soc_api.c
+++ drivers/media/platform/msm/camera_v2/common/cam_soc_api.c
@@ -473,16 +473,16 @@ EXPORT_SYMBOL(msm_camera_put_clk_info_and_rates);
 
 /* Get regulators from DT */
 int msm_camera_get_regulator_info(struct platform_device *pdev,
-				struct msm_cam_regulator **vdd_info,
+				struct regulator ***vdd,
 				int *num_reg)
 {
 	uint32_t cnt;
 	int i, rc;
 	struct device_node *of_node;
+	const char *name;
 	char prop_name[32];
-	struct msm_cam_regulator *tmp_reg;
 
-	if (!pdev || !vdd_info || !num_reg)
+	if (!pdev || !vdd || !num_reg)
 		return -EINVAL;
 
 	of_node = pdev->dev.of_node;
@@ -499,95 +499,82 @@ int msm_camera_get_regulator_info(struct platform_device *pdev,
 		return -EINVAL;
 	}
 
-	tmp_reg = devm_kcalloc(&pdev->dev, cnt,
-				sizeof(struct msm_cam_regulator), GFP_KERNEL);
-	if (!tmp_reg)
+	*num_reg = cnt;
+	(*vdd) = devm_kcalloc(&pdev->dev, cnt, sizeof(struct regulator *),
+				GFP_KERNEL);
+	if (!*vdd)
 		return -ENOMEM;
 
 	for (i = 0; i < cnt; i++) {
 		rc = of_property_read_string_index(of_node,
-			"qcom,vdd-names", i, &tmp_reg[i].name);
+			"qcom,vdd-names", i, &name);
 		if (rc < 0) {
 			pr_err("Fail to fetch regulators: %d\n", i);
 			rc = -EINVAL;
 			goto err1;
 		}
 
-		CDBG("regulator-names[%d] = %s\n", i, tmp_reg[i].name);
+		CDBG("regulator-names[%d] = %s\n", i, name);
 
-		snprintf(prop_name, 32, "%s-supply", tmp_reg[i].name);
+		snprintf(prop_name, 32, "%s-supply", name);
 
 		if (of_get_property(of_node, prop_name, NULL)) {
-			tmp_reg[i].vdd =
-				devm_regulator_get(&pdev->dev, tmp_reg[i].name);
-			if (IS_ERR(tmp_reg[i].vdd)) {
+			(*vdd)[i] = devm_regulator_get(&pdev->dev, name);
+			if (IS_ERR((*vdd)[i])) {
 				rc = -EINVAL;
 				pr_err("Fail to get regulator :%d\n", i);
 				goto err1;
 			}
 		} else {
-			pr_err("Regulator phandle not found :%s\n",
-				tmp_reg[i].name);
-			rc = -EINVAL;
+			pr_err("Regulator phandle not found :%s\n", name);
 			goto err1;
 		}
-		CDBG("vdd ptr[%d] :%p\n", i, tmp_reg[i].vdd);
+		CDBG("vdd ptr[%d] :%pK\n", i, (*vdd)[i]);
 	}
-
-	*num_reg = cnt;
-	*vdd_info = tmp_reg;
 
 	return 0;
 
 err1:
 	for (--i; i >= 0; i--)
-		devm_regulator_put(tmp_reg[i].vdd);
-	devm_kfree(&pdev->dev, tmp_reg);
+		devm_regulator_put((*vdd)[i]);
+	devm_kfree(&pdev->dev, *vdd);
 	return rc;
 }
 EXPORT_SYMBOL(msm_camera_get_regulator_info);
 
 
 /* Enable/Disable regulators */
-int msm_camera_regulator_enable(struct msm_cam_regulator *vdd_info,
+int msm_camera_regulator_enable(struct regulator **vdd,
 				int cnt, int enable)
 {
 	int i;
 	int rc;
-	struct msm_cam_regulator *tmp = vdd_info;
 
-	if (!tmp) {
+	CDBG("cnt : %d, enable : %d\n", cnt, enable);
+	if (!vdd) {
 		pr_err("Invalid params");
 		return -EINVAL;
 	}
-	CDBG("cnt : %d\n", cnt);
 
 	for (i = 0; i < cnt; i++) {
-		if (tmp && !IS_ERR_OR_NULL(tmp->vdd)) {
-			CDBG("name : %s, enable : %d\n", tmp->name, enable);
-			if (enable) {
-				rc = regulator_enable(tmp->vdd);
-				if (rc < 0) {
-					pr_err("regulator enable failed %d\n",
-						i);
-					goto error;
-				}
-			} else {
-				rc = regulator_disable(tmp->vdd);
-				if (rc < 0)
-					pr_err("regulator disable failed %d\n",
-						i);
+		if (enable) {
+			rc = regulator_enable(vdd[i]);
+			if (rc < 0) {
+				pr_err("regulator enable failed %d\n", i);
+				goto error;
 			}
+		} else {
+			rc = regulator_disable(vdd[i]);
+			if (rc < 0)
+				pr_err("regulator disable failed %d\n", i);
 		}
-		tmp++;
 	}
 
 	return 0;
 error:
 	for (--i; i > 0; i--) {
-		--tmp;
-		if (!IS_ERR_OR_NULL(tmp->vdd))
-			regulator_disable(tmp->vdd);
+		if (!IS_ERR_OR_NULL(vdd[i]))
+			regulator_disable(vdd[i]);
 	}
 	return rc;
 }
@@ -595,23 +582,24 @@ EXPORT_SYMBOL(msm_camera_regulator_enable);
 
 /* Put regulators regulators */
 void msm_camera_put_regulators(struct platform_device *pdev,
-	struct msm_cam_regulator **vdd_info, int cnt)
+							struct regulator ***vdd,
+							int cnt)
 {
 	int i;
 
-	if (!vdd_info || !*vdd_info) {
+	if (!*vdd) {
 		pr_err("Invalid params\n");
 		return;
 	}
 
 	for (i = cnt - 1; i >= 0; i--) {
-		if (vdd_info[i] && !IS_ERR_OR_NULL(vdd_info[i]->vdd))
-			devm_regulator_put(vdd_info[i]->vdd);
-			CDBG("vdd ptr[%d] :%p\n", i, vdd_info[i]->vdd);
+		if (!IS_ERR_OR_NULL((*vdd)[i]))
+			devm_regulator_put((*vdd)[i]);
+			CDBG("vdd ptr[%d] :%pK\n", i, (*vdd)[i]);
 	}
 
-	devm_kfree(&pdev->dev, *vdd_info);
-	*vdd_info = NULL;
+	devm_kfree(&pdev->dev, *vdd);
+	*vdd = NULL;
 }
 EXPORT_SYMBOL(msm_camera_put_regulators);
 
@@ -655,11 +643,12 @@ EXPORT_SYMBOL(msm_camera_register_irq);
 int msm_camera_register_threaded_irq(struct platform_device *pdev,
 			struct resource *irq, irq_handler_t handler_fn,
 			irq_handler_t thread_fn, unsigned long irqflags,
-			const char *irq_name, void *dev_id)
+			char *irq_name, void *dev_id)
 {
 	int rc = 0;
 
-	if (!pdev || !irq || !irq_name || !dev_id) {
+	if (!pdev || !irq || !handler_fn || !thread_fn ||
+		!irq_name || !dev_id) {
 		pr_err("Invalid params\n");
 		return -EINVAL;
 	}
